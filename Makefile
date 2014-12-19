78f10da56bb433efcd90566d4cb00b4c1df7423f
na2hiro <a@81.la>
2014-12-20 01:02:23 +0900

    連結し名前空間内へ

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	cat pre.js CharacterObject.js GameGraphicsForApplet.js GameKey.js GameMouse.js GameSoundForApplet.js GlobalFunctions.js IdouGamen.js ImageBuff.js KeyboardMenu.js MainProgram.js MapSystem.js MasaoConstruction.js TagDataBase.js YukaObject.js post.js > CanvasMasao.js
---
+++ post.js
@@ -0,0 +1,17 @@
+	var CanvasMasao = {};
+	CanvasMasao.CharacterObject = CharacterObject;
+	CanvasMasao.GameGraphicsForApplet = GameGraphicsForApplet;
+	CanvasMasao.GameKey = GameKey;
+	CanvasMasao.GameMouse = GameMouse;
+	CanvasMasao.GameSoundForApplet = GameSoundForApplet;
+	CanvasMasao.Game = Game;
+	CanvasMasao.IdouGamen = IdouGamen;
+	CanvasMasao.ImageBuff = ImageBuff;
+	CanvasMasao.KeyboardMenu = KeyboardMenu;
+	CanvasMasao.MainProgram = MainProgram;
+	CanvasMasao.MapSystem = MapSystem;
+	CanvasMasao.MasaoConstruction = MasaoConstruction;
+	CanvasMasao.TagDataBase = TagDataBase;
+	CanvasMasao.YukaObject = YukaObject;
+	return CanvasMasao;
+}();
