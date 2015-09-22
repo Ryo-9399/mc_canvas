08b47796b919dba0f403bcc4985ac1c89197b367
uhyo <uhyo@uhyohyo.net>
2015-09-22 18:56:06 +0900

    emit complete input data

--- Extends/InputRecorder.js
+++ Extends/InputRecorder.js
@@ -2,10 +2,19 @@
 //
 
 /*
- * Input binary format:
+ * Masao Playlog Format: METADATA (HEADER BODY)*
+ *
+ * METADATA block:
+ *   magic: 0x4D 0x0E 0x50 0x0A
+ *   version 4 octets | MPF version in Big-Endian (0x00 0x00 0x00 0x01)
+ *   reserved: 0x00 0x00 0x00 0x0C
  *
  * HEADER block:
  *   seed: 4 octets | initial ran_seed of succeeding BODY block in Big-Endian
+ *   stage: 1 octet | stage number
+ *   status:1 octet | 0x00 = aborted/miss, 0x01 = cleared
+ *   reserved: 0x00 0x00
+ *   score: 4 octets| eventual score of this log
  *   length: 4 octets | length in bytes of succeeding BODY block in Big-Endian
  *
  * BODY block:
@@ -29,6 +38,7 @@ CanvasMasao.InputRecorder = (function(){
         this.stage=null;    //現在のステージ
         this.recording = false;
         //バッファ
+        this.allbuf=[]; //全ステージ通しのデータが入ったバッファ
         this.buffers=[];
         this.current_buffer=null;
         this.current_buffer_size=0;
@@ -94,51 +104,111 @@ CanvasMasao.InputRecorder = (function(){
             this.recording=true;
         }else if(prev_playing && !playing){
             //ステージが終わった
-            if(this.inputdataCallback != null){
-                //全てのバッファをまとめる
-                var result_buffers=this.buffers.concat(this.current_buffer.subarray(0,this.current_buffer_size));
-                //lengthを計算
-                var buffer_length = 0;
-                result_buffers.forEach(function(buf){
-                    buffer_length += buf.length * buf.BYTES_PER_ELEMENT;
-                });
-                //HEADER blockを作成
-                var header = new Uint8Array(8);
-                var header_view = new DataView(header.buffer);
-                //seed
-                header_view.setUint32(0, this.ran_seed, false);
-                //length
-                header_view.setUint32(4, buffer_length, false);
-                //HEADER blockを結合
-                result_buffers = [header].concat(result_buffers);
 
-                //状況を表すオブジェクトを作る
-                //種類(クリアした="clear", やられた="miss", 中断した="abort")
-                var status;
-                if(mc.mp.stage !== this.stage || ml_mode===260 || ml_mode >= 400){
-                    //ステージが進行した or ゲームクリア画面なのでステージクリアした
-                    status="clear";
-                }else if(ml_mode>=300 || ml_mode===90 || ml_mode===250){
-                    //ゲームオーバー画面 or ステージ開始画面
-                    status="miss";
-                }else{
-                    status="abort";
-                }
-                var result={
-                    status: status,
-                    //入力データが表すステージ(1-4)
-                    stage: this.stage,
-                    //最終スコア
-                    score: mc.mp.score,
-                    buffers: result_buffers
-                };
+            //全てのバッファをまとめる
+            var result_buffers=this.buffers.concat(this.current_buffer.subarray(0,Math.ceil(this.current_buffer_size/4)*4));
+            //lengthを計算
+            var buffer_length = 0;
+            result_buffers.forEach(function(buf){
+                buffer_length += buf.length * buf.BYTES_PER_ELEMENT;
+            });
+            //種類(クリアした="clear", やられた="miss", 中断した="abort")
+            var status;
+            if(mc.mp.stage !== this.stage || ml_mode===260 || ml_mode >= 400){
+                //ステージが進行した or ゲームクリア画面なのでステージクリアした
+                status="clear";
+            }else if(ml_mode>=300 || ml_mode===90 || ml_mode===250){
+                //ゲームオーバー画面 or ステージ開始画面
+                status="miss";
+            }else{
+                status="abort";
+            }
+            //最終スコア
+            var score=mc.mp.score;
+
+            //HEADER blockを作成
+            var header = new Uint8Array(16);
+            var header_view = new DataView(header.buffer);
+            //seed
+            header_view.setUint32(0, this.ran_seed, false);
+            //stage
+            header_view.setUint8(4, this.stage);
+            //status
+            header_view.setUint8(5, status==="clear" ? 1 : 0);
+            //reserved
+            header_view.setUint16(6,0);
+            //score
+            header_view.setUint32(8, score, false);
+            //length
+            header_view.setUint32(12, buffer_length, false);
+            //HEADER blockを結合
+            result_buffers = [header].concat(result_buffers);
+
+            //状況を表すオブジェクトを作る
+            var result={
+                status: status,
+                //入力データが表すステージ(1-4)
+                stage: this.stage,
+                //最終スコア
+                score: score,
+                buffer: this.singlify(result_buffers)
+            };
+            if(this.inputdataCallback != null){
                 this.inputdataCallback(result);
+            }
+            //記録
+            if(status==="clear"){
+                //クリアのログを残す
+                this.allbuf = this.allbuf.concat(result_buffers);
+                if(ml_mode>=400 && this.inputdataCallback != null){
+                    //全部終わったので結果をアレする
+                    //METADATA blockを作る
+                    var metadata = new Uint8Array([0x4D, 0x0E, 0x50, 0x0A,
+                                                  0x00, 0x00, 0x00, 0x01,
+                                                  0x00, 0x00, 0x00, 0x0C
+                    ]);
+                    this.allbuf = [metadata].concat(this.allbuf);
+                    setTimeout(function(){
+                        this.inputdataCallback({
+                            status: "all",
+                            stage: this.stage,
+                            score: score,
+                            buffer: this.singlify(this.allbuf)
+                        });
+                    }.bind(this),0);
+                }
+            }
+            if(ml_mode===50){
+                //タイトルに戻った
+                this.allbuf = [];
             }
             this.recording=false;
         }else{
             this.frame++;
         }
         this.prev_ml_mode = ml_mode;
+    };
+    InputRecorder.prototype.singlify = function(buffers){
+        //buffersはじつはTypedArrayの配列
+        //バッファのサイズを計算する
+        for(var sum=0, i=0, l=buffers.length; i<l; i++){
+            var buf=buffers[i];
+            sum+=buf.length*buf.BYTES_PER_ELEMENT;
+        }
+        //全て
+        var result = new ArrayBuffer(sum);
+        var buf1, buf2;
+        //コピーする
+        for(sum=0, i=0; i<l; i++){
+            buf=buffers[i];
+            var size=buf.length*buf.BYTES_PER_ELEMENT;
+            //Uint8Arrayを介してコピー
+            buf1=new Uint8Array(result, sum, size);
+            buf2=new Uint8Array(buf.buffer, 0, size);
+            buf1.set(buf2);
+            sum+=size;
+        }
+        return result;
     };
     InputRecorder.inject = function(mc, options){
         var _ui=mc.userInit, _us=mc.userSub;
