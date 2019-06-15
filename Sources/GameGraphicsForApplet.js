7f87aee59bb19a4fc4833e6b8a6b151376f62f42
Tatamo <nsorder@gmail.com>
2019-06-15 18:38:56 +0900

    add doccomment to all methods in GameGraphics

--- Sources/GameGraphicsForApplet.js
+++ Sources/GameGraphicsForApplet.js
@@ -24,18 +24,38 @@ class GameGraphicsForApplet {
 		this.mode = 2;
 		this.oya = null;
 		this.di = new Dimension(512, 320);
-		this.mt = undefined;
+		/**
+		 * 背景色
+		 * @type {Color}
+		 */
 		this.backcolor = Color.black;
+		/**
+		 * タイトル画像などの一枚画像を格納する配列
+		 * @type {Array<ImageBuff>}
+		 */
+		this.li = new Array(10);
+		/**
+		 * メディアトラッカー。画像が全て読み込み完了するのを待つために用いる
+		 * @type {Array<ImageBuff>}
+		 */
+		this.mt = [];
 		this.os_img = this.ap.createImage(this.di.width, this.di.height);
+		/**
+		 * ゲーム画面のグラフィック
+		 * @type {Graphics}
+		 */
 		this.os_g = this.os_img.getGraphics();
 		this.os_g_bk = this.os_img.getGraphicsBk();
 		this.os2_img = this.ap.createImage(this.di.width + 96, this.di.height + 96);
+		/**
+		 * 裏画面のグラフィック
+		 * 裏画面は描画予定のマップ上のブロックや地図画面を保持する
+		 * @type {Graphics}
+		 */
 		this.os2_g = this.os2_img.getGraphics();
 		this.os32_img = this.ap.createImage(32, 32);
 		this.os32_g = this.os32_img.getGraphics();
-		this.apt_img = undefined;
 		this.pg = undefined;
-		this.li = new Array(10);
 		this.spt_img = createNDimensionArray(2, this.spt_kazu);
 		this.hi = undefined;
 		this.spt_option_img = new Array(10);
@@ -52,7 +72,6 @@ class GameGraphicsForApplet {
 		} else {
 			this.layer_mode = 1;
 		}
-		this.mt = [];
 
 		str = this.tdb.getValue("filename_pattern");
 		this.apt_img = this.ap.getImage(str);
@@ -67,6 +86,9 @@ class GameGraphicsForApplet {
 		}
 	}
 
+	/**
+	 * パターン画像を32x32ピクセルごとに切り分ける
+	 */
 	cut() {
 		// ■■■32x32にカットする処理
 		var i, j, n, m, localG;
@@ -135,8 +157,13 @@ class GameGraphicsForApplet {
 		}
 	}
 
-	setPatternImage(paramString) {
-		this.apt_img = this.ap.getImage(paramString);
+	/**
+	 * 画像を読み込み、新たなパターン画像とする
+	 * @param {string} filename ファイル名(または画像のURL,相対パス)
+	 */
+	setPatternImage(filename) {
+		this.apt_img = this.ap.getImage(filename);
+		// TODO: ap.getImageがnullを返すことはあるのか？
 		// イメージを読み込めなかったときは操作を無効化するハック
 		if (this.apt_img == null) return;
 
@@ -160,11 +187,15 @@ class GameGraphicsForApplet {
 		}
 	}
 
-	setMapchipImage(paramString) {
+	/**
+	 * 画像を読み込み、新たなマップチップ画像とする
+	 * @param {string} filename ファイル名(または画像のURL,相対パス)
+	 */
+	setMapchipImage(filename) {
 		if (this.layer_mode != 2) {
 			return;
 		}
-		this.amapchip_img = this.ap.getImage(paramString);
+		this.amapchip_img = this.ap.getImage(filename);
 		// ハック
 		if (this.amapchip_img == null) return;
 
@@ -185,107 +216,200 @@ class GameGraphicsForApplet {
 		}
 	}
 
-	addListImage(paramInt, paramString) {
-		this.li[paramInt] = this.ap.getImage(paramString);
-
-		this.mt.push(this.li[paramInt]);
+	/**
+	 * 画像を読み込み、指定された番号の画像として格納する
+	 * @param {number} id 画像番号
+	 * @param {string} filename ファイル名(または画像のURL,相対パス)
+	 */
+	addListImage(id, filename) {
+		this.li[id] = this.ap.getImage(filename);
+		this.mt.push(this.li[id]);
 	}
 
-	addListImage2(paramInt, paramString) {
-		this.li[paramInt] = this.ap.getImage(paramString);
+	/**
+	 * 画像を読み込み、指定された番号の画像として格納する
+	 * 読み込みの完了を監視しない
+	 * @param {number} id 画像番号
+	 * @param {string} filename ファイル名(または画像のURL,相対パス)
+	 */
+	addListImage2(id, filename) {
+		this.li[id] = this.ap.getImage(filename);
 	}
 
+	/**
+	 * 画像の読み込み完了を待つ
+	 * NOTE: 現在未実装。何もしない
+	 */
 	loadListImage() {
 		waitFor(this.mt);
 	}
 
-	loadImage(paramString) {
-		var localImage = null;
+	/**
+	 * 外部ファイルから画像を読み込み、ImageBuffオブジェクトを返す
+	 * NOTE: 本来は読み込み完了まで待つが、待つ処理は現在実装されていない
+	 * @param {string} filename ファイル名(または画像のURL,相対パス)
+	 * @returns {ImageBuff|null} 読み込めない場合はnullを返す
+	 */
+	loadImage(filename) {
+		let image = null;
 		try {
-			localImage = this.ap.getImage(paramString);
-		} catch (ex1) {
-			localImage = null;
+			image = this.ap.getImage(filename);
+		} catch (e) {
+			// TODO:ここのエラーハンドリングの見直し
 		}
-		if (localImage == null) {
+		if (image == null) {
+			// 読み込めない場合はnullを返す
 			return null;
 		}
-		var localMediaTracker = [];
-		localMediaTracker.push(localImage);
-		waitFor(localMediaTracker);
-		return localImage;
+		const mt = [image];
+		waitFor(mt);
+		return image;
 	}
 
-	copyOS(paramGraphics) {
-		paramGraphics.drawImage(this.os_img, 0, 0, this.ap);
+	/**
+	 * 指定されたGraphicsオブジェクトに現在のゲーム画面を描画する
+	 * @param {Graphics} graphics 描画先Graphicsオブジェクト
+	 */
+	copyOS(graphics) {
+		graphics.drawImage(this.os_img, 0, 0, this.ap);
 	}
 
+	/**
+	 * 画面を背景色で塗りつぶす
+	 */
 	fill() {
 		this.os_g.setColor(this.backcolor);
 		this.os_g.fillRect(0, 0, this.di.width, this.di.height);
 	}
 
+	/**
+	 * 裏画面を背景色で塗りつぶす
+	 */
 	fill2() {
 		this.os2_g.setColor(this.backcolor);
-
 		this.os2_g.fillRect(0, 0, this.di.width + 96, this.di.height + 96);
 	}
 
-	setBackcolor(paramColor) {
-		this.backcolor = paramColor;
+	/**
+	 * 背景色を変更する
+	 * @param {Color} color
+	 */
+	setBackcolor(color) {
+		this.backcolor = color;
 	}
 
-	drawPT(paramInt1, paramInt2, paramInt3, paramInt4) {
-		this.os_g.drawImage(this.spt_img[paramInt4][paramInt3], paramInt1, paramInt2, this.ap);
+	/**
+	 * 画面上にパターン画像を描画する
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 * @param {number} muki [0,1] 1なら左右反転
+	 */
+	drawPT(wx, wy, pt, muki) {
+		this.os_g.drawImage(this.spt_img[muki][pt], wx, wy, this.ap);
 	}
 
-	drawPattern(paramInt1, paramInt2, paramInt3, paramInt4) {
+	/**
+	 * 裏画面上にパターン画像を描画する
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 */
+	drawPT2(wx, wy, pt) {
+		this.os2_g.drawImage(this.hi[pt], wx, wy, this.ap);
+	}
+
+	/**
+	 * 画面上にパターン画像を描画する
+	 * NOTE: drawPTとの違いとして、mukiが0,1以外の値をとる場合でも描画失敗とならずに描画を行う
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 * @param {number} muki 1なら左右反転
+	 */
+	drawPattern(wx, wy, pt, muki) {
 		var i = 0;
-		if (paramInt4 == 1) {
+		if (muki == 1) {
 			i = 1;
 		}
-		this.os_g.drawImage(this.spt_img[i][paramInt3], paramInt1, paramInt2, this.ap);
+		this.os_g.drawImage(this.spt_img[i][pt], wx, wy, this.ap);
 	}
 
-	drawPatternCut(paramInt1, paramInt2, paramInt3, paramInt4, paramInt5) {
+	/**
+	 * 画面上にパターン画像を描画するが、上からdyピクセルの範囲は描画しない
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 * @param {number} muki 1なら左右反転
+	 * @param {number} dy 描画しない部分の高さ
+	 */
+	drawPatternCut(wx, wy, pt, muki, dy) {
 		var i = 0;
-		if (paramInt4 == 1) {
+		if (muki == 1) {
 			i = 1;
 		}
-		this.os32_g.drawImage(this.os_img, paramInt1 * -1, paramInt2 * -1 - paramInt5, this.ap);
+		this.os32_g.drawImage(this.os_img, wx * -1, wy * -1 - dy, this.ap);
 
-		this.os32_g.drawImage(this.spt_img[i][paramInt3], 0, paramInt5 * -1, this.ap);
+		this.os32_g.drawImage(this.spt_img[i][pt], 0, dy * -1, this.ap);
 
-		this.os_g.drawImage(this.os32_img, paramInt1, paramInt2 + paramInt5, this.ap);
+		this.os_g.drawImage(this.os32_img, wx, wy + dy, this.ap);
 	}
 
-	drawPT2(paramInt1, paramInt2, paramInt3) {
-		this.os2_g.drawImage(this.hi[paramInt3], paramInt1, paramInt2, this.ap);
-	}
-
-	drawBG2(paramInt1, paramInt2, paramInt3) {
+	/**
+	 * 裏画面上にパターン画像を描画する
+	 * ただしパターン画像の透過部分を背景色で塗りつぶす
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 */
+	drawBG2(wx, wy, pt) {
 		this.os2_g.setColor(this.backcolor);
-		this.os2_g.fillRect(paramInt1, paramInt2, 32, 32);
-
-		this.os2_g.drawImage(this.hi[paramInt3], paramInt1, paramInt2, this.ap);
+		this.os2_g.fillRect(wx, wy, 32, 32);
+		this.os2_g.drawImage(this.hi[pt], wx, wy, this.ap);
 	}
 
-	drawBG3(paramInt1, paramInt2, paramInt3, paramColor) {
-		this.os2_g.setColor(paramColor);
-		this.os2_g.fillRect(paramInt1, paramInt2, 32, 32);
-
-		this.os2_g.drawImage(this.hi[paramInt3], paramInt1, paramInt2, this.ap);
+	/**
+	 * 裏画面上にパターン画像を描画する
+	 * ただしパターン画像の透過部分を指定した色で塗りつぶす
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 * @param {Color} color 背後の色
+	 */
+	drawBG3(wx, wy, pt, color) {
+		this.os2_g.setColor(color);
+		this.os2_g.fillRect(wx, wy, 32, 32);
+		this.os2_g.drawImage(this.hi[pt], wx, wy, this.ap);
 	}
 
-	drawMapchip(paramInt1, paramInt2, paramInt3) {
-		this.os_g.drawImage(this.smapchip_img[paramInt3], paramInt1, paramInt2, this.ap);
+	/**
+	 * 画面上にマップチップ画像を描画する
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} code パターンコード
+	 */
+	drawMapchip(wx, wy, code) {
+		this.os_g.drawImage(this.smapchip_img[code], wx, wy, this.ap);
 	}
 
-	drawMapchip2(paramInt1, paramInt2, paramInt3) {
-		this.os2_g.drawImage(this.smapchip_img[paramInt3], paramInt1, paramInt2, this.ap);
+	/**
+	 * 裏画面上にマップチップ画像を描画する
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} code パターンコード
+	 */
+	drawMapchip2(wx, wy, code) {
+		this.os2_g.drawImage(this.smapchip_img[code], wx, wy, this.ap);
 	}
 
-	drawListImage(paramInt1, paramInt2, paramInt3) {
-		this.os_g.drawImage(this.li[paramInt3], paramInt1, paramInt2, this.ap);
+	/**
+	 * 画面上にタイトル画面などの一枚画像を貼り付ける
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} id [0,this.li.length) 画像番号
+	 */
+	drawListImage(wx, wy, id) {
+		this.os_g.drawImage(this.li[id], wx, wy, this.ap);
 	}
 }
 
--- Sources_v28/GameGraphics.js
+++ Sources_v28/GameGraphics.js
@@ -20,20 +20,44 @@ class GameGraphics {
 		 */
 		this.spt_kazu = this.spt_kazu_x * this.spt_kazu_y;
 		this.spt_h_kijyun = 3;
-		this.li = new Array(10);
-		this.spt_img = createNDimensionArray(2, this.spt_kazu);
 		this.di = new Dimension(512, 320);
+		/**
+		 * 背景色
+		 * @type {Color}
+		 */
 		this.backcolor = Color.black;
+		/**
+		 * タイトル画像などの一枚画像を格納する配列
+		 * @type {Array<ImageBuff>}
+		 */
+		this.li = new Array(10);
+		/**
+		 * メディアトラッカー。画像が全て読み込み完了するのを待つために用いる
+		 * @type {Array<ImageBuff>}
+		 */
+		this.mt = [];
 		this.os_img = this.ap.createImage(this.di.width, this.di.height);
+		/**
+		 * ゲーム画面のグラフィック
+		 * @type {Graphics}
+		 */
 		this.os_g = this.os_img.getGraphics();
 		this.os2_img = this.ap.createImage(this.di.width + 96, this.di.height + 96);
+		/**
+		 * 裏画面のグラフィック
+		 * 裏画面は描画予定のマップ上のブロックや地図画面を保持する
+		 * @type {Graphics}
+		 */
 		this.os2_g = this.os2_img.getGraphics();
-		this.mt = [];
+		this.spt_img = createNDimensionArray(2, this.spt_kazu);
 
 		var str = this.ap.getParameter("filename_pattern");
 		this.apt_img = this.ap.getImage(str);
 	}
 
+	/**
+	 * パターン画像を32x32ピクセルごとに切り分ける
+	 */
 	cut() {
 		// ■■■32x32にカットする処理
 		var i, j, i2, i1, n, m, localG;
@@ -56,53 +80,106 @@ class GameGraphics {
 		this.hi = this.spt_img[0];
 	}
 
-	addListImage(i, s) {
-		this.li[i] = this.ap.getImage(s);
-		this.mt.push(this.li[i]);
+	/**
+	 * 画像を読み込み、指定された番号の画像として格納する
+	 * @param {number} id 画像番号
+	 * @param {string} filename ファイル名(または画像のURL,相対パス)
+	 */
+	addListImage(id, filename) {
+		this.li[id] = this.ap.getImage(filename);
+		this.mt.push(this.li[id]);
 	}
 
 	loadImage() {}
 
-	copyOS(g) {
-		g.drawImage(this.os_img, 0, 0, this.ap);
+	/**
+	 * 指定されたGraphicsオブジェクトに現在のゲーム画面を描画する
+	 * @param {Graphics} graphics 描画先Graphicsオブジェクト
+	 */
+	copyOS(graphics) {
+		graphics.drawImage(this.os_img, 0, 0, this.ap);
 	}
 
+	/**
+	 * 画面を背景色で塗りつぶす
+	 */
 	fill() {
 		this.os_g.setColor(this.backcolor);
 		this.os_g.fillRect(0, 0, this.di.width, this.di.height);
 	}
 
+	/**
+	 * 裏画面を背景色で塗りつぶす
+	 */
 	fill2() {
 		this.os2_g.setColor(this.backcolor);
 		this.os2_g.fillRect(0, 0, this.di.width + 96, this.di.height + 96);
 	}
 
+	/**
+	 * 背景色を変更する
+	 * @param {Color} color
+	 */
 	setBackcolor(color) {
 		this.backcolor = color;
 	}
 
-	drawPT(i, j, k, l) {
-		this.os_g.drawImage(this.spt_img[l][k], i, j, this.ap);
+	/**
+	 * 画面上にパターン画像を描画する
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 * @param {number} muki [0,1] 1なら左右反転
+	 */
+	drawPT(wx, wy, pt, muki) {
+		this.os_g.drawImage(this.spt_img[muki][pt], wx, wy, this.ap);
 	}
 
-	drawPT2(i, j, k) {
-		this.os2_g.drawImage(this.hi[k], i, j, this.ap);
+	/**
+	 * 裏画面上にパターン画像を描画する
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 */
+	drawPT2(wx, wy, pt) {
+		this.os2_g.drawImage(this.hi[pt], wx, wy, this.ap);
 	}
 
-	drawBG2(i, j, k) {
+	/**
+	 * 裏画面上にパターン画像を描画する
+	 * ただしパターン画像の透過部分を背景色で塗りつぶす
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 */
+	drawBG2(wx, wy, pt) {
 		this.os2_g.setColor(this.backcolor);
-		this.os2_g.fillRect(i, j, 32, 32);
-		this.os2_g.drawImage(this.hi[k], i, j, this.ap);
+		this.os2_g.fillRect(wx, wy, 32, 32);
+		this.os2_g.drawImage(this.hi[pt], wx, wy, this.ap);
 	}
 
-	drawBG3(i, j, k, color) {
+	/**
+	 * 裏画面上にパターン画像を描画する
+	 * ただしパターン画像の透過部分を指定した色で塗りつぶす
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} pt [0,spt_kazu) パターンコード
+	 * @param {Color} color 背後の色
+	 */
+	drawBG3(wx, wy, pt, color) {
 		this.os2_g.setColor(color);
-		this.os2_g.fillRect(i, j, 32, 32);
-		this.os2_g.drawImage(this.hi[k], i, j, this.ap);
+		this.os2_g.fillRect(wx, wy, 32, 32);
+		this.os2_g.drawImage(this.hi[pt], wx, wy, this.ap);
 	}
 
-	drawListImage(i, j, k) {
-		this.os_g.drawImage(this.li[k], i, j, this.ap);
+	/**
+	 * 画面上にタイトル画面などの一枚画像を貼り付ける
+	 * @param {number} wx 画面上のX座標
+	 * @param {number} wy 画面上のY座標
+	 * @param {number} id [0,this.li.length) 画像番号
+	 */
+	drawListImage(wx, wy, id) {
+		this.os_g.drawImage(this.li[id], wx, wy, this.ap);
 	}
 }
 
