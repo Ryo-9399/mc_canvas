f9fbc09b7c6b3f255dcdb7c1828c93dccbaed8dc
urotaichi <pcdij@yahoo.co.jp>
2020-03-17 14:46:11 +0900

    apply rounddown

--- Sources/CharacterObject/Boss.js
+++ Sources/CharacterObject/Boss.js
@@ -1,4 +1,5 @@
 import { CharacterObject } from "../CharacterObject";
+import { rounddown } from "../GlobalFunctions";
 
 // ボスの状態
 export const DYING = 40;
@@ -563,8 +564,8 @@ class Boss extends CharacterObject {
 				// 反転しても角度が同じ場合は一発しか出さない
 				if (degree_normalized !== degree_inversed) rads.push((degree_inversed * 3.14) / 180);
 				for (const rad of rads) {
-					const cos = Math.round(Math.cos(rad) * 12);
-					const sin = Math.round(Math.sin(rad) * 10);
+					const cos = rounddown(Math.cos(rad) * 12);
+					const sin = rounddown(Math.sin(rad) * 10);
 					mp.mSet2(this.x, this.y, 710, cos, sin);
 				}
 				break;
@@ -620,8 +621,8 @@ class Boss extends CharacterObject {
 		for (let i = 0; i < 8; i++) {
 			// NOTE: 後方互換性のためMath.PI等ではなく3.14を用いてラジアンに変換する
 			const d = (normalizeDegree(i * 45 + degree) * 3.14) / 180;
-			const cos = Math.round(Math.cos(d) * 8);
-			const sin = -Math.round(Math.sin(d) * 8);
+			const cos = rounddown(Math.cos(d) * 8);
+			const sin = -rounddown(Math.sin(d) * 8);
 			mp.mSet2(this.x, this.y - 8, 710, cos, sin);
 			mp.gs.rsAddSound(18);
 		}
@@ -674,8 +675,8 @@ class Boss extends CharacterObject {
 			}
 		}
 		if (rad !== null) {
-			const cos = Math.round(Math.cos(rad) * 12);
-			const sin = Math.round(Math.sin(rad) * 8);
+			const cos = rounddown(Math.cos(rad) * 12);
+			const sin = rounddown(Math.sin(rad) * 8);
 			mp.mSet2(this.x, this.y, 711, cos, sin);
 		}
 
