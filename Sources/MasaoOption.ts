b9efb7a138197e1ca2d87442348ea10933e1108b
Ryo-9399 <ryo19950926@gmail.com>
2021-01-02 01:31:13 +0900

    起動オプションに関するコメントを MasaoOption に移動

--- Sources/MasaoOption.ts
+++ Sources/MasaoOption.ts
@@ -11,17 +11,33 @@ export type Params = { [K in KnownParameterKey]: string } & {
 export interface Option {
 	width?: number;
 	height?: number;
+	/** ハイスコア更新時に呼び出されるコールバック関数 */
 	highscoreCallback?(score: number): void;
+	/** 毎フレーム呼び出されるコールバック関数 */
 	userJSCallback?(Offscreen_g: Graphics, mode: number, view_x: number, view_y: number, ap: MasaoJSS): void;
+	/** 拡張機能 */
 	extensions?: Extension[];
+	/**
+	 * 第3版マップデータ
+	 * （内部的には `advanced-map` を優先して使う）
+	 * @deprecated
+	 */
 	"advance-map"?: AdvancedMap;
+	/** 第3版マップデータ */
 	"advanced-map"?: AdvancedMap;
+	/** 敵の数制限の後方互換性を保つ */
 	"bc-enemy-number"?: boolean;
+	/** ループに必ずsetIntervalを使う */
 	"bc-loop-setinterval"?: boolean;
+	/** Web Audio APIを使わない音声再生を行う */
 	"bc-no-webaudio"?: boolean;
+	/** Web Audio APIを使う場合でも同じ効果音を重複して再生しない */
 	"bc-no-overlap-sound"?: boolean;
+	/** 拡張JSのメソッドの大文字小文字を区別しない */
 	"bc-case-insensitive"?: boolean;
+	/** 小数切り捨て処理をJava版の挙動にする */
 	"bc-use-rounddown"?: boolean;
+	/** メインループを行うためのLoopクラスです。（テスト用） */
 	"custom-loop"?: ILoop;
 }
 
